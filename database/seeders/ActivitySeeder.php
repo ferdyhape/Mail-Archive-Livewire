dfb2121c2e23bb0e420bd9721d53526d2762cc81
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-02-08 16:25:04 +0700

    add activity model migration seeder

---
+++ database/seeders/ActivitySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Activity;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 11; $i++) {
+            Activity::create([
+                'name' => 'Activity-' . $i,
+            ]);
+        }
+    }
+}
