312b8aeaa7250f3fe0eb293dc36ec35a2bce026c
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-02-08 16:21:45 +0700

    add menu for archive button

---
+++ app/Http/Livewire/Dashboard/Menu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class Menu extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.menu');
+    }
+}
