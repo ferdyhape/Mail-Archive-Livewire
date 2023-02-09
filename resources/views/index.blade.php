2e3a2470786e9c43612cee674a6b90655464870f
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-02-10 01:40:36 +0700

    add button menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,15 +5,22 @@
 
         @if (Auth::check())
         <h2>Mail Archive</h2>
-        <div class="d-flex justify-content-between">
-            <div class="d-flex gap-2">
-                @livewire('dashboard.archive.create')
-                @livewire('dashboard.category.create')
-            </div>
-            @livewire('auth.logout')
+        <div class="d-flex gap-2 my-3">
+            <button class="btn btn-info" onclick="archiveshow()">Archive</button>
+            <button class="btn btn-info" onclick="categoryshow()">Category</button>
+            <button class="btn btn-info" onclick="activityshow()">Activity</button>
         </div>
-        @livewire('dashboard.table')
+        <div id="archive-display" style="display: none;">
+            @livewire('dashboard.archive.index')
+        </div>
+        <div id="category-display" style="display: none;">
+            @livewire('dashboard.category.index')
+        </div>
+        <div id="activity-display" style="display: none;">
+            @livewire('dashboard.activity.index')
+        </div>
 
+        @livewire('auth.logout')
         @else
         @livewire('auth.login')
 
@@ -26,4 +33,21 @@
         font-family: 'Source Sans Pro', sans-serif;
     }
 </style>
+<script>
+    function archiveshow() {
+        document.getElementById("category-display").style.display = "none";
+        document.getElementById("activity-display").style.display = "none";
+        document.getElementById("archive-display").style.display = "block";
+    }
+    function categoryshow() {
+        document.getElementById("activity-display").style.display = "none";
+        document.getElementById("archive-display").style.display = "none";
+        document.getElementById("category-display").style.display = "block";
+    }
+    function activityshow() {
+        document.getElementById("category-display").style.display = "none";
+        document.getElementById("archive-display").style.display = "none";
+        document.getElementById("activity-display").style.display = "block";
+    }
+</script>
 @endsection
